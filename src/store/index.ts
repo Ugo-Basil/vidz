a206702479e44b82f4164d48b415c71a5740431d
Basil <ohuruogubasil@gmail.com>
2023-07-15 15:34:29 +0100

    added:sidebar

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,7 @@
 import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
+
+import { InitialState } from "../Types";
+// import { getHomePageVideos } from "./reducers/getHomePageVideos";
+// import { getRecommendedVideos } from "./reducers/getRecommendedVideos";
+// import { getSearchPageVideos } from "./reducers/getSearchPageVideos";
+// import { getVideoDetails } from "./reducers/getVideoDetails";
